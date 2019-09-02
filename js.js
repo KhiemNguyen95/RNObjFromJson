a8a7d20d31c60230633c10f5c62e5ef728f70d50
Khiemnv <>
2019-09-02 22:19:48 +0700

    get obj on obj

--- js.js
+++ js.js
@@ -20,26 +20,37 @@ export const #userObj = {
 
 function getData(){
 	let inputText = document.getElementById("inputJson").value;
+	let NameObj = document.getElementById("objName").value;
+		// 
+	consoleJson(NameObj,inputText)
+}
+
+function consoleJson(nameObj,dataIn){
 	try 
 	{
-		let inputJson = JSON.parse(inputText)
+		let inputJson = JSON.parse(dataIn)
+		//get key
 		var keys = [];
 		for(var k in inputJson) keys.push(k);
-
+		if (keys.length == 0) {
+			return
+		}
+		// out put param
 		let outKey = "";
 		keys.forEach(item => {
 			outKey = outKey + "    " + item + ": '"+ item + "',\n";
+			consoleJson(item,inputJson[item]);
 		});
-
-		let NameObj = document.getElementById("objName").value;
-		let jsonOut = exampleOut.replace("#userObj",NameObj)
+		//fill data to example
+		let jsonOut = exampleOut
+		jsonOut = jsonOut.replace("#userObj",nameObj)
 		jsonOut = jsonOut.replace("#key",outKey)
-		// 
+		// log result
+		console.log("name obj:" + nameObj + ".js")
 		console.log(jsonOut);
 	}
-	catch
+	catch (err) 
 	{
-		//document.getElementById("outputJson").value = "looix";
+		//console.log(err);
 	}
-	//console.log(jsonout);
 }
